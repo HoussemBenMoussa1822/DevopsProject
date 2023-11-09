6dc71dad3df803d0c5a887336a2d36e7a8533744
HoussemBenMoussa1822 <75402749+HoussemBenMoussa1822@users.noreply.github.com>
2023-11-10 00:50:16 +0100

    Update GestionStationSkiApplication.java

--- src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
+++ src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
@@ -4,10 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
 
 @SpringBootApplication
 @EnableScheduling
 public class GestionStationSkiApplication {
+	@Configuration
+	@EnableSwagger2
+	public class SwaggerConfig {
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2);
+		}
+	}
 
 	public static void main(String[] args)
 	{
